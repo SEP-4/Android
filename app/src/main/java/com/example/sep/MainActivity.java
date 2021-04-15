a0267b2511a9dc1e9e31b991e9bd973df8acd269
vladBobeica <vlad.bobeica@outlook.com>
2021-04-15 12:56:36 +0200

    add login functionality

--- app/src/main/java/com/example/sep/MainActivity.java
+++ app/src/main/java/com/example/sep/MainActivity.java
@@ -1,14 +1,65 @@
 package com.example.sep;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
+    Button login;
+    EditText email, password;
+
+    FirebaseAuth auth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        auth = FirebaseAuth.getInstance();
+        email = (EditText) findViewById(R.id.emailEdit);
+        password = (EditText) findViewById(R.id.passwordEdit);
+        login = (Button)findViewById(R.id.loginBtn);
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email1 = email.getText().toString();
+                String password1 = password.getText().toString();
+
+                LoginUser(password1, email1);
+            }
+        });
+
+
     }
+
+    private void LoginUser(String password, String email) {
+        auth.signInWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()) {
+                    Toast.makeText(MainActivity.this, "Login completed.", Toast.LENGTH_SHORT).show();
+
+                }
+                else {
+                    Toast.makeText(MainActivity.this, "Error 404", Toast.LENGTH_SHORT).show();
+
+                }
+            }
+        });
+
+    }
+
 }
